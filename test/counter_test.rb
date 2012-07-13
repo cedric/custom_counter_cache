388675820fc7cd9d5e80eef181913e2c47e6713c
Aaron Suggs <aaron@ktheory.com>
2012-07-13 11:42:22 -0400

    Add tests for eager_loading counter

--- test/counter_test.rb
+++ test/counter_test.rb
@@ -5,18 +5,18 @@ class CounterTest < Test::Unit::TestCase
   def setup
     @user = User.create
   end
-  
+
   def test_default_counter_value
     assert_equal 0, @user.published_count
   end
-  
+
   def test_create_and_destroy_counter
     @user.articles.create(:state => 'published')
     assert_equal 1, Counter.count
     @user.destroy
     assert_equal 0, Counter.count
   end
-  
+
   def test_increment_and_decrement_counter_with_conditions
     @article = @user.articles.create(:state => 'unpublished')
     assert_equal 0, @user.published_count
@@ -29,5 +29,17 @@ class CounterTest < Test::Unit::TestCase
   end
 
   # Test that an eager loaded
-  
+  def test_eager_loading_with_no_counter
+    @article = @user.articles.create(:state => 'unpublished')
+    user = User.includes(:counters).first
+    assert_equal 0, user.published_count
+
+  end
+
+  def test_eager_loading_with_counter
+    @article = @user.articles.create(:state => 'published')
+    @user = User.includes(:counters).find(@user.id)
+    assert_equal 1, @user.published_count
+  end
+
 end
